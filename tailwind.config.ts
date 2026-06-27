62fa3f244d60c21276bf69e729c07e41d75e0d16
Arbaj Ansari <arbaj897ansari@gmail.com>
2026-06-27 20:39:35 +0530

    d

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,6 +79,10 @@ const config = {
           from: { transform: "translateY(0)" },
           to: { transform: "translateY(calc(-100% - var(--gap)))" },
         },
+        shimmer: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(100%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
